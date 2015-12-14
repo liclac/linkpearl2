7bf8a22c26d8f201dd6c4d790a8b2193cac53365
uppfinnarn <uppfinnarn@gmail.com>
2015-12-14 19:29:55 +0100

    Admin pages

--- linkpearl_lodestone/admin.py
+++ linkpearl_lodestone/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from linkpearl_lodestone.models import Server, Title, Character
 
-# Register your models here.
+@admin.register(Server)
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'server', 'title')
