e8c7e4d26f0f9d3244cda48c0f9a9c017e46d405
uppfinnarn <uppfinnarn@gmail.com>
2015-12-14 20:21:29 +0100

    Update Character data from the admin

--- linkpearl_lodestone/admin.py
+++ linkpearl_lodestone/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from linkpearl_lodestone.models import Server, Title, Character
+from linkpearl_lodestone.parsers import CharacterParser
 
 @admin.register(Server)
 class ServerAdmin(admin.ModelAdmin):
@@ -12,3 +13,9 @@ class TitleAdmin(admin.ModelAdmin):
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'server', 'title')
+    actions = ['update']
+    
+    def update(self, request, queryset):
+        p = CharacterParser()
+        for char in queryset:
+            p.update(char)
