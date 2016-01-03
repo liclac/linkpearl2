a8aed247c6cb35d4f14c6fda0d6b22fb6f595204
uppfinnarn <uppfinnarn@gmail.com>
2016-01-03 17:16:01 +0100

    Reordering fields

--- web/app/models/server.js
+++ web/app/models/server.js
@@ -3,6 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   slug: DS.attr('string'),
-  population: DS.attr('number'),
+
   characters: DS.hasMany('character'),
+
+  population: DS.attr('number'),
 });
