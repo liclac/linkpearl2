11cbfb001fd7054c90463c3fed67585210cf892b
uppfinnarn <uppfinnarn@gmail.com>
2016-01-03 02:03:13 +0100

    Server population info

--- linkpearl_lodestone/serializers.py
+++ linkpearl_lodestone/serializers.py
@@ -4,6 +4,11 @@ from linkpearl_lodestone.models import Server, Character
 class ServerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Server
+    
+    population = serializers.SerializerMethodField()
+    
+    def get_population(self, obj):
+        return obj.characters.count()
 
 class CharacterSerializer(serializers.ModelSerializer):
     class Meta:
--- web/app/models/server.js
+++ web/app/models/server.js
@@ -3,4 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   slug: DS.attr('string'),
+  population: DS.attr('number'),
 });
