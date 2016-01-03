8c28bff39034d00cb7088acd40b48ca017dd027d
uppfinnarn <uppfinnarn@gmail.com>
2016-01-03 16:50:22 +0100

    Show the server on character pages

--- web/app/models/character.js
+++ web/app/models/character.js
@@ -4,4 +4,5 @@ export default DS.Model.extend({
   lodestone_id: DS.attr('number'),
   first_name: DS.attr('string'),
   last_name: DS.attr('string'),
+  server: DS.belongsTo('server', { async: true }),
 });
--- web/app/models/server.js
+++ web/app/models/server.js
@@ -4,4 +4,5 @@ export default DS.Model.extend({
   name: DS.attr('string'),
   slug: DS.attr('string'),
   population: DS.attr('number'),
+  characters: DS.hasMany('character'),
 });
