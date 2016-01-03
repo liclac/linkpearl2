3cd03364eec1e03a9d1594298c04f4b7a2b047c9
uppfinnarn <uppfinnarn@gmail.com>
2016-01-03 23:23:48 +0100

    Sorted minions and mounts

--- web/app/controllers/characters/show.js
+++ web/app/controllers/characters/show.js
@@ -3,4 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   sortedLevelsBy: ['level:desc', 'exp_at:desc', 'name'],
   sortedLevels: Ember.computed.sort('model.levels', 'sortedLevelsBy'),
+  sortedMinionsBy: ['name'],
+  sortedMinions: Ember.computed.sort('model.minions', 'sortedMinionsBy'),
+  sortedMountsBy: ['name'],
+  sortedMounts: Ember.computed.sort('model.mounts', 'sortedMountsBy'),
 });
