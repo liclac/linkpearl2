6323e1ba77b0ccbf7f2a086d4957afdfa32e0f2a
uppfinnarn <uppfinnarn@gmail.com>
2016-01-04 18:59:33 +0100

    Array.reduce is useful

--- web/app/controllers/servers/index.js
+++ web/app/controllers/servers/index.js
@@ -4,14 +4,8 @@ export default Ember.Controller.extend({
   sortedServersBy: ['population:desc', 'name'],
   sortedServers: Ember.computed.sort('model', 'sortedServersBy'),
   populationData: Ember.computed('model', function() {
-    let model = this.get('model');
-    let data = [];
-    this.get('model').forEach(function(server) {
-      data.push({
-        value: server.get('population'),
-        label: server.get('name'),
-      });
+    return this.get('model').map(function(server) {
+      return { value: server.get('population'), label: server.get('name') };
     });
-    return data;
   }),
 });
