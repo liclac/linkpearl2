754f67e849347105c5dc2858173942d1acf3abd3
uppfinnarn <uppfinnarn@gmail.com>
2016-01-05 19:00:32 +0100

    Reset scroll in the component

--- web/app/components/character-table.js
+++ web/app/components/character-table.js
@@ -3,4 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   page: 1,
   model: null,
+  resetScroll: Ember.observer('page', function() {
+    window.scrollTo(0, 0);
+  }),
 });
--- web/app/controllers/characters/index.js
+++ web/app/controllers/characters/index.js
@@ -3,7 +3,4 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   queryParams: ['page'],
   page: 1,
-  resetScroll: Ember.observer('page', function() {
-    window.scrollTo(0, 0);
-  }),
 });
