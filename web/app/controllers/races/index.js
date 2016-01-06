0b2a34716ae3a4675cd52c35eafa57ec5b186c28
uppfinnarn <uppfinnarn@gmail.com>
2016-01-06 19:37:04 +0100

    Better way to handle racial data

--- web/app/controllers/races/index.js
+++ web/app/controllers/races/index.js
@@ -1,28 +1,54 @@
 import Ember from 'ember';
+import _ from 'lodash/lodash';
 
 export default Ember.Controller.extend({
   splitByClan: false,
 
   tableData: Ember.computed('model.races', 'model.stats', 'splitByClan', function() {
+    // Reduce races into a plain object for quick id lookups
+    let races = this.get('model.races').reduce((val, race) => {
+      val[race.get('id')] = {
+        id: race.get('id'),
+        name: race.get('name'),
+        slug: race.get('slug'),
+        clans: [
+          { name: race.get('clan_1'), num_characters: [0, 0] },
+          { name: race.get('clan_2'), num_characters: [0, 0] },
+        ],
+        num_characters: race.get('num_characters'),
+      };
+      return val;
+    }, {});
+
+    // Fill in statistics information
+    this.get('model.stats').forEach((stat) => {
+      let race = races[stat.race];
+      let clan = race.clans[stat.clan - 1];
+      clan.num_characters[stat.gender - 1] += stat.num_characters;
+    });
+
+    // Make it into an array of values
+    let data = _.values(races);
+
+    // If we're splitting by clan, split the data by clans
     if (this.get('splitByClan')) {
-      return this.get('model.stats').map((stat) => {
-        let race = this.get('model.races').findBy('id', stat.race.toString());
-        return {
-          id: race.get('id'),
-          name: race.get('name') + " - " + race.get('clan_' + stat.clan),
-          slug: race.get('slug'),
-          num_characters: stat.num_characters,
-          race_num_characters: race.get('num_characters'),
-          get: function(key) { return this[key]; }
-        }
-      }).sortBy('race_num_characters').reverse();
-    } else {
-      return this.get('model.races').sortBy('num_characters').reverse();
+      let newData = [];
+      data.forEach((race) => {
+        race.clans.forEach((clan) => {
+          let d = _.clone(race);
+          d.name += ' - ' + clan.name;
+          d.num_characters = clan.num_characters.reduce((cur, val) => { return cur + val; });
+          newData.push(d);
+        });
+      });
+      data = newData;
     }
+
+    return data;
   }),
   chartData: Ember.computed('tableData', function() {
     return this.get('tableData').map((race) => {
-      return { label: race.get('name'), value: race.get('num_characters') };
+      return { label: race.name, value: race.num_characters };
     });
   }),
 });
