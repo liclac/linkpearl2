8fc5d5e49720cbb4fc0dffbeb4369667ef81c514
uppfinnarn <uppfinnarn@gmail.com>
2016-01-07 11:42:29 +0100

    Non-sticky page numbers

--- web/app/routes/characters/index.js
+++ web/app/routes/characters/index.js
@@ -3,7 +3,12 @@ import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 
 export default Ember.Route.extend(RouteMixin, {
   perPage: 100,
+  resetController: function(controller, isExiting/*, transition*/) {
+    if (isExiting) {
+      controller.set('page', 1);
+    }
+  },
   model: function() {
     return this.findPaged('character', {});
-  }
+  },
 });
