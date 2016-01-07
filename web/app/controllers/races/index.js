bf7f31f1f6bac4bfe7df1ef0d44cc394c5cb4136
uppfinnarn <uppfinnarn@gmail.com>
2016-01-07 08:57:21 +0100

    Much better split algorithm

--- web/app/controllers/races/index.js
+++ web/app/controllers/races/index.js
@@ -6,78 +6,64 @@ export default Ember.Controller.extend({
   splitByGender: false,
   filterByRace: null,
 
-  tableData: Ember.computed('model.races', 'model.stats', 'splitByClan', 'splitByGender', 'filterByRace', function() {
-    // Reduce races into a plain object for quick id lookups
-    let races = this.get('model.races').reduce((val, race) => {
-      val[race.get('id')] = {
-        id: race.get('id'),
-        name: race.get('name'),
-        slug: race.get('slug'),
-        clans: [
-          { name: race.get('clan_1'), num_characters: [0, 0] },
-          { name: race.get('clan_2'), num_characters: [0, 0] },
-        ],
-        num_characters: race.get('num_characters'),
+  data: Ember.computed('model.races', 'model.stats', 'splitByClan', 'splitByGender', 'filterByRace', function() {
+    let data = this.get('model.races').map((race) => {
+      return {
+        race: race,
+        stats: this.get('model.stats').filterBy('race', parseInt(race.get('id'))),
       };
-      return val;
-    }, {});
-
-    // Fill in statistics information
-    this.get('model.stats').forEach((stat) => {
-      let race = races[stat.race];
-      let clan = race.clans[stat.clan - 1];
-      clan.num_characters[stat.gender - 1] += stat.num_characters;
     });
 
-    // Make it into an array of values
-    let data = _.values(races);
-
-    // If requested, filter the data by race
     if (this.get('filterByRace')) {
-      let filterID = this.get('filterByRace.id');
-      data = data.filter((race) => {
-        return race.id === filterID;
-      });
+      data = data.filterBy('race.id', this.get('filterByRace.id'));
     }
 
-    // If requested, split the data by clans
     if (this.get('splitByClan')) {
-      let newData = [];
-      data.forEach((race) => {
-        race.clans.forEach((clan) => {
-          let d = _.clone(race);
-          d.name += ' - ' + clan.name;
-          d.num_characters = clan.num_characters.reduce((cur, val) => { return cur + val; });
-          d._clan = clan;
-          newData.push(d);
+      data = data.reduce((cur, val) => {
+        [1, 2].forEach((clan_id) => {
+          let d = _.clone(val);
+          d.clan = { id: clan_id, name: d.race.get('clan_' + clan_id) };
+          d.stats = d.stats.filterBy('clan', clan_id);
+          cur.push(d);
         });
-      });
-      data = newData;
+        return cur;
+      }, []);
     }
 
-    // If requested, split the data by genders
     if (this.get('splitByGender')) {
-      let newData = [];
-      data.forEach((race) => {
-        ['\u2642', '\u2640'].forEach((symbol, gender) => {
-          let d = _.clone(race);
-          d.name += ' ' + symbol;
-          if (d._clan) {
-            d.num_characters = d._clan.num_characters[gender];
-          } else {
-            d.num_characters = d.clans.reduce((cur, clan) => { return cur + clan.num_characters[gender]; }, 0);
-          }
-          newData.push(d);
+      let genders = [
+        { id: 1, name: "Male", symbol: '\u2642' },
+        { id: 2, name: "Female", symbol: '\u2640' },
+      ];
+      data = data.reduce((cur, val) => {
+        genders.forEach((gender) => {
+          let d = _.clone(val);
+          d.gender = gender;
+          d.stats = d.stats.filterBy('gender', gender.id);
+          cur.push(d);
         });
-      });
-      data = newData;
+        return cur;
+      }, []);
     }
 
-    return data;
+    return data.map((d) => {
+      d.num_characters = d.stats.reduce((cur, stat) => {
+        cur += stat.num_characters;
+        return cur;
+      }, 0);
+      return d;
+    });
   }),
-  chartData: Ember.computed('tableData', function() {
-    return this.get('tableData').map((race) => {
-      return { label: race.name, value: race.num_characters };
+  chartData: Ember.computed('data', function() {
+    return this.get('data').map((d) => {
+      let label = d.race.get('name');
+      if (d.clan) {
+        label += ' - ' + d.clan.name;
+      }
+      if (d.gender) {
+        label += ' ' + d.gender.symbol;
+      }
+      return { label: label, value: d.num_characters };
     });
   }),
 });
